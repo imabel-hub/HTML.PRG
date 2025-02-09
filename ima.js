e95a80c15c23b168b9fd06c97730478e0858040d
imene belferkous <eevangeline861@gmail.com>
2025-02-09 16:15:01 +0100

    Mise en dev Blog

--- ima.js
+++ ima.js
@@ -1,4 +1,7 @@
 let currmoletile;
+let currplantile;
+let score = 0;
+let gameover = false;
 
 
 window.onload = function() {
@@ -12,12 +15,14 @@ function setGame() {
 
         let tile = document.createElement("div");
         tile.id = i.toString();
+        tile.addEventListener("click",selectTile);
         document.getElementById("board").appendChild(tile);
 
         
     }
 
-    setInterval(setmole,2000);
+    setInterval(setmole,1000);
+    setInterval(setPlant,2000);
     
 }
 
@@ -28,6 +33,10 @@ function getrandomtile(){
 
 function setmole () {
 
+    if(gameover){
+        return;
+    }
+
     if (currmoletile) {
         currmoletile.innerHTML ="";
     }
@@ -36,14 +45,56 @@ let mole = document.createElement("img");
 mole.src ="./MontyMole.webp";
 
 let num = getrandomtile();
+if (currplantile && currplantile.id == num) {
+    return;
+}
+
 
     currmoletile = document.getElementById(num);
     currmoletile.appendChild(mole);
 
 
 
+}
+function setPlant() {
 
+    if(gameover){
+        return;
+    }
 
+    if (currplantile) {
+        currplantile.innerHTML = ""; // Nettoyer le contenu précédent
+    }
 
+    const plant = document.createElement("img");
+    plant.src = "./Piranha_Plant_3D_Land.webp";
+    
+    let num = getrandomtile();
+
+    if (currmoletile && currmoletile.id == num) {
+        return;
+    }
+    
+       
+    
+    currplantile = document.getElementById(num);
+    
+        currplantile.appendChild(plant);
 
 }
+
+function selectTile() {
+
+    if(gameover){
+        return;
+    }
+    if (this == currmoletile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString(); //update score
+    } 
+    else if (this == currplantile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameover = true;
+    }
+}
+
